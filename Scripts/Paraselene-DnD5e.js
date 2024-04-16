2e8dca9aafe85e54b9d69dfc12b8c451d71f3753
Neil Luna <nluna@paraselene.com>
2024-04-16 14:32:29 -0500

    25 - Created D&D 5e specific script.

---
+++ Scripts/Paraselene-DnD5e.js
@@ -0,0 +1,147 @@
+// Paraselene-DnD5e
+// Dungeons & Dragons 5th Edition tools.
+// Version 1.0.0
+
+// Github:   https://github.com/neilluna
+// By:       Neil Luna
+// Contact:  https://app.roll20.net/users/280391/neil-luna
+
+// Register the offset of the start of this script.
+var API_Meta = API_Meta || {};
+API_Meta.ParaseleneDnD5e = {
+    offset: Number.MAX_SAFE_INTEGER,
+    lineCount: -1,
+    version: '1.0.0',
+};
+{
+    const errorLineNumber = 19;  // Set this to the line number of the "throw new Error('')" below.
+    try {
+        throw new Error('');  // Set errorLineNumber (above) to this line number.
+    }
+    catch (exeception) {
+        const reportedLineNumber = parseInt(exeception.stack.split(/\n/)[1].replace(/^.*:(\d+):.*$/, '$1'), 10);
+        const offset = reportedLineNumber - errorLineNumber;
+        API_Meta.ParaseleneDnD5e.offset = offset;
+    }
+}
+
+const ParaseleneDnD5e = (() => {
+
+    const scriptName = 'Paraselene-DnD5e';
+    const version = API_Meta.ParaseleneDnD5e.version;
+    const schemaVersion = '1.0.0';
+    const scriptAuthor = 'Neil Luna';
+ 
+    // Convenience aliases for ParaseleneCommon.
+    let pc = null;
+    let Attribute = null;
+    let Element = null;
+    let Table = null;
+    let Row = null;
+    let Header = null;
+    let Cell = null;
+    let Link = null;
+
+    // Log the version info.
+    const versionInfo = () => {
+        log(`-=> ${scriptName} - ${version} by ${scriptAuthor} <=- Meta offset: ${API_Meta.ParaseleneDnD5e.offset}`);
+    };
+
+    // Check the schema version and update if necessary.
+    const checkSchema = () => {
+        if (!state.hasOwnProperty(scriptName) || state[scriptName].version !== schemaVersion) {
+            log(`  > Updating schema to version ${schemaVersion} <`);
+
+            switch (state[scriptName] && state[scriptName].version) {
+                case '1.0.0':
+                    // No break statement. This must fall through.
+
+                case 'UpdateSchemaVersion':
+                    state[scriptName].version = schemaVersion;
+                    break;
+
+                default:
+                    state[scriptName] = {
+                        version: schemaVersion,
+                    };
+                    break;
+            }
+        }
+    };
+
+    // Check if Paraselene-Common is loaded.
+    const isParseleneCommonLoaded = () => {
+        if (pc !== null) {
+            return true;
+        }
+        if (API_Meta.ParaseleneCommon === undefined) {
+            sendChat(speakAs, `/w "${playerName}" <br/>Paraselene-Common is not loaded.`, null, { noarchive: true });
+            return false;
+        }
+
+        // Set up the convenience aliases for ParaseleneCommon.
+        pc = ParaseleneCommon;
+        Attribute = pc.HtmlAttribute;
+        Element = pc.HtmlElement;
+        Table = pc.HtmlBorderedTable;
+        Row = pc.HtmlBorderedTableRow;
+        Header = pc.HtmlBorderedTableHeader;
+        Cell = pc.HtmlBorderedTableCell;
+        Link = pc.HtmlLink;
+
+        return true;
+    };
+
+    // Say hello to the world.
+    const helloWorld = (msg) => {
+        if (!isParseleneCommonLoaded()) {
+            return;
+        }
+
+        const commandName = `${scriptName}-Hello-World`;
+        const args = msg.content.split(/\s+/);
+        if (msg.type != 'api' || args[0] != `!${commandName}`) {
+            return;
+        }
+        const speakAs = pc.extractCommandLineOption(args, '--speakAs', commandName);
+
+        const playerId = msg.playerid
+        const player = getObj('player', playerId);
+        const playerName = player.get('displayname');
+
+        pc.sendChatNoArchive(speakAs, `/w "${playerName}" <br/>Hello World.`);
+    };
+
+    // Register event handlers.
+    const registerEventHandlers = () => {
+        on('chat:message', helloWorld);
+    };
+
+    // When all scripts have loaded ...
+    on('ready', () => {
+        versionInfo();
+        checkSchema();
+        registerEventHandlers();
+    });
+
+    // Public interface.
+    return {
+        version: version,
+    };
+
+})();  // ParaseleneDnD5e
+
+// Register the length of this script.
+{
+    // Set this to the number of lines following the "throw new Error('')" below.
+    const numberOfLinesFromErrorToScriptEnd = 9;
+    try {
+        throw new Error('');  // Set numberOfLinesFromErrorToScriptEnd to the number of lines following this line.
+    }
+    catch (exception) {
+        const reportedLineNumber = parseInt(exception.stack.split(/\n/)[1].replace(/^.*:(\d+):.*$/, '$1'), 10);
+        const offset = API_Meta.ParaseleneDnD5e.offset;
+        const lineCount = reportedLineNumber - offset + numberOfLinesFromErrorToScriptEnd;
+        API_Meta.ParaseleneDnD5e.lineCount = lineCount;
+    }
+}
